a25a6be4059b83cc91ee3faa75387b887ff33306
Andrew Basson <andrew.basson@gmail.com>
2025-10-06 14:22:21 +0200

    fix: add version command and arg
    Signed-off-by: Andrew Basson <andrew.basson@gmail.com>

--- Sources/Container-Compose/Application.swift
+++ Sources/Container-Compose/Application.swift
@@ -10,11 +10,14 @@ import ArgumentParser
 
 @main
 struct Main: AsyncParsableCommand {
+    static let version: String = "v0.5.1"
     static let configuration: CommandConfiguration = .init(
         commandName: "container-compose",
         abstract: "A tool to use manage Docker Compose files with Apple Container",
+        version: Self.version,
         subcommands: [
             ComposeUp.self,
-            ComposeDown.self
+            ComposeDown.self,
+            Version.self
         ])
 }
